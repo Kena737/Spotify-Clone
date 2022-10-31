a749955d8775b869679e8338999d0ff35e3f6b1b
Kena <75708839+Kena737@users.noreply.github.com>
2022-10-30 23:02:08 -0400

    Nothing Spacial

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -1,10 +1,18 @@
-import { ChevronRight, ChevronLeft  } from "react-iconly"
+import { ChevronRight, ChevronLeft, ChevronDown, User  } from "react-iconly"
 const Navbar = () => {
   return (
-    <div className='py-4 px-5 flex justify-between bg-black '>
+    <div className='py-4 px-5 flex items-center justify-between bg-black'>
       <div className="space-x-5 flex">
-        <div className="bg-[#080808] rounded-full p-2 "><ChevronLeft set="bold" primaryColor="white"/></div>
-        <div className="bg-[#080808] rounded-full p-2 "><ChevronRight set="bold" primaryColor="white"/></div>
+        <div className="bg-[#131212] rounded-full p-2 "><ChevronLeft set="bold" primaryColor="white"/></div>
+        <div className="bg-[#131212] rounded-full p-2 "><ChevronRight set="bold" primaryColor="white"/></div>
+      </div>
+      <div className="flex space-x-9">
+          <button className="py-2 px-4 text-sm font-bold border border-gray-500 rounded-full">Upgrade</button>
+        <div className="bg-[#131212] flex items-center rounded-full px-1">
+          <div className="p-1 bg-zinc-500 rounded-full"><User set="light" primaryColor="white"/></div>
+          <div className="text-sm font-bold pl-2">Kena</div>
+          <div><ChevronDown set="bold" primaryColor="white"/></div>
+        </div>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -7,51 +7,85 @@ export default function Home() {
   return (
     <div className="bg-neutral-900 text-white">
       <div className='flex'>
-        <div className='bg-black'><Sidebar /></div>
+        <div className='bg-black'>
+          <Sidebar />
+        </div>
         <div>
-          <Navbar />
+          <div className='w-full'><Navbar/></div>
           <div className='p-10'>
-            <h1 className='text-xl font-bold'>Recently played</h1>
-            <div id='Card' className='flex flex-wrap '>
-              <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
-                <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={180} width={180} />
-                <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
-                <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+            <div className='py-5'>
+              <h1 className='text-xl font-bold'>Recently played</h1>
+              <div id='Card' className='flex flex-wrap '>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
               </div>
-              <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
-                <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={180} width={180} />
-                <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
-                <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+            </div>
+            <div className='py-5'>
+              <h1 className='text-xl font-bold'>Recently played</h1>
+              <div id='Card' className='flex flex-wrap '>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
               </div>
-              <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
-                <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={180} width={180} />
-                <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
-                <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
-              </div>
-              <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
-                <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={180} width={180} />
-                <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
-                <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
-              </div>
-              <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
-                <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={180} width={180} />
-                <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
-                <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
-              </div>
-              <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
-                <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={180} width={180} />
-                <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
-                <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
-              </div>
-              <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
-                <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={180} width={180} />
-                <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
-                <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
-              </div>
-              <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
-                <Image className='rounded-md hover:shadow-xl' src="https://i.scdn.co/image/ab67706f000000023676fcd91c7f3e300ccd6deb" alt='Thumbnail' height={180} width={180} />
-                <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
-                <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+            </div>
+            <div className='py-5'>
+              <h1 className='text-xl font-bold'>Popular artists</h1>
+              <div id='Card' className='flex flex-wrap '>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-full hover:shadow-xl' src="https://i.scdn.co/image/ab6761610000f1780261696c5df3be99da6ed3f3" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-full hover:shadow-xl' src="https://i.scdn.co/image/ab6761610000f1780261696c5df3be99da6ed3f3" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-full hover:shadow-xl' src="https://i.scdn.co/image/ab6761610000f1780261696c5df3be99da6ed3f3" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-full hover:shadow-xl' src="https://i.scdn.co/image/ab6761610000f1780261696c5df3be99da6ed3f3" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
+                <div className='bg-neutral-900 hover:bg-neutral-800 rounded-lg shadow-xl p-4 w-52 m-3'>
+                  <Image className='rounded-full hover:shadow-xl' src="https://i.scdn.co/image/ab6761610000f1780261696c5df3be99da6ed3f3" alt='Thumbnail' height={150} width={150} />
+                  <h3 className='font-bold py-3'>Hip Hop Hindi</h3>
+                  <p className='text-sm text-gray-300'>Hottest Hindi music served here. Cover. Ranbir & Alia </p>
+                </div>
               </div>
             </div>
           </div>
